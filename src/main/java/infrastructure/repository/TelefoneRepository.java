f782e09a730741746c356559257ca00699586dd2
Cjocar <167594403+Cjocar@users.noreply.github.com>
2025-01-11 22:47:13 -0300

    implementação de security

---
+++ src/main/java/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,12 @@
+package infrastructure.repository;
+
+
+import com.javanauta.aprendendospring.infrastructure.entity.Telefone;
+import com.javanauta.aprendendospring.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone,Long> {
+
+}
